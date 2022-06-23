bec7e6ec7fd7682c24d6dd02d25101bef0356f97
sen006 <sen006@uib.no>
2022-06-23 03:14:02 +0200

    [ADD] Added the beginning of a hash table
    Signed-off-by: sen006 <sen006@uib.no>

---
+++ src/Main/HashTable/Table.java
@@ -0,0 +1,12 @@
+package Main.HashTable;
+
+public class Table {
+        int[] primes = {1009, 10007, 100003, 1000003, 1000000019};
+        int size;
+        int R = 31;
+
+        public Table(int s, int r){
+            
+        }
+
+}
